df9903aef3063eba880e225e62cdd45743a0bae1
KIM DONG WON <35540229+ehddnjs198@users.noreply.github.com>
2024-09-19 17:40:26 +0900

    Add files via upload

---
+++ DelegateStudy3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    delegate int FuncDelegate(int Num1, int Num2);
+    class Counter
+    {
+        private int threshold;
+        private int total;
+
+        public Counter(int passedThreshold)
+        {
+            threshold = passedThreshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                args.Threshold = threshold;
+                args.TimeReached = DateTime.Now;
+                OnThresholdReached(args);
+            }
+        }
+
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+            if(handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+
+        //public event EventHandler<>
+        public class ThresholdReachedEventArgs : EventArgs
+        {
+            public int Threshold { get; set; }
+            public DateTime TimeReached { get; set; }
+        }
+    }
+
+    class DelegateStudy3
+    {
+        static void c_ThresholdReadhed(object sender, EventArgs e)
+        {
+            Console.WriteLine("The threshold was reached");
+            Environment.Exit(0);
+        }
+
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(new Random().Next(10));
+            c.ThresholdReached += c_ThresholdReadhed;
+
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
