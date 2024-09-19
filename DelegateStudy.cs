a6759bd79c030aa5071e8b26abb89e8d4beb5b9c
KIM DONG WON <35540229+ehddnjs198@users.noreply.github.com>
2024-09-19 17:03:13 +0900

    Add files via upload

---
+++ DelegateStudy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    delegate int FuncDelegate(int Num1, int Num2);
+
+
+    class DelegateStudy
+    {
+        static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        static void Main(string[] args)
+        {
+            FuncDelegate plusDelegate = Plus;
+            FuncDelegate minusDelegate = Minus;
+
+
+            Console.WriteLine(plusDelegate(5,10));
+            Console.WriteLine(minusDelegate(10, 5));
+            Console.ReadLine();
+        }
+    }
+}
